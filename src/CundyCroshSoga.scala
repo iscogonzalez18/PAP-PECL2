ade6f927f4439db814ead66125d29f14e60be6eb
Isco <83782270+iscogonzalez18@users.noreply.github.com>
2023-04-05 15:17:52 +0200

    clase datosCundyCroshSoga done

--- src/CundyCroshSoga.scala
+++ src/CundyCroshSoga.scala
@@ -2,9 +2,8 @@ object CundyCroshSoga {
 
     def main(args: Array[String]): Unit = {
         println("Introduce las opciones de ejecución (ej: css -a 2 10 50):")
-        val opciones = scala.io.StdIn.readLine()
         // creamos el objeto de la clase
-        val opcionesJuego:datosCundyCroshSoga = new datosCundyCroshSoga(opciones)
-        opcionesJuego.filtrarDatos()
+        val opcionesJuego = datosCundyCroshSoga(scala.io.StdIn.readLine())
+        println("To string " + opcionesJuego.filas + " " + opcionesJuego.columnas + " " + opcionesJuego.dificultad + " " + opcionesJuego.modo)
     }
 }
--- src/datosCCS.scala
+++ src/datosCCS.scala
@@ -1,21 +1,25 @@
-case class datosCundyCroshSoga(opciones: String) {
-  require(opcionesFiltradas.length != 5)
-  
-  def filtrarDatos(): List[String] = {
-    // filtramos la entrada de datos
-    val opcionesFiltradas = opciones.split(" ").filter(_.nonEmpty)
-    var listaOpciones: List[String] = List()
-    // comprobamos que la entrada de datos es correcta
-    if () {
-      println("Error en la entrada de datos")
-      throw new Error("Error en la entrada de datos")
-    } else {
-      listaOpciones = List(opcionesFiltradas(1).substring(1, 2), opcionesFiltradas(2), opcionesFiltradas(3), opcionesFiltradas(4))
-    }
-    if ((listaOpciones.head != "a" && listaOpciones.head != "m") || !listaOpciones(1).forall(_.isDigit) || listaOpciones(1).toInt > 2 || listaOpciones(1).toInt < 1 || !listaOpciones(2).forall(_.isDigit) || !listaOpciones(3).forall(_.isDigit)) {
-      println("Error en la entrada de datos")
-      throw new Error("Error en la entrada de datos")
-    }
-    listaOpciones
+case class datosCundyCroshSoga(opciones: String, modo: String, dificultad: Int, filas: Int, columnas: Int)
+object datosCundyCroshSoga {
+  def apply(opciones: String): datosCundyCroshSoga = {
+    val datos = filtrarDatos(opciones)
+    datosCundyCroshSoga(opciones, datos(0), datos(1).toInt, datos(2).toInt, datos(3).toInt)
   }
+}
+
+def filtrarDatos(opciones: String) : List[String] = {
+  // filtramos la entrada de datos
+  val opcionesFiltradas = opciones.split(" ").filter(_.nonEmpty)
+  var listaOpciones: List[String] = List()
+  // comprobamos que la entrada de datos es correcta
+  if (opcionesFiltradas.length != 5){
+    println("Error en la entrada de datos")
+    throw new Error("Error en la entrada de datos")
+  } else {
+    listaOpciones = List(opcionesFiltradas(1).substring(1, 2), opcionesFiltradas(2), opcionesFiltradas(3), opcionesFiltradas(4))
+  }
+  if ((listaOpciones.head != "a" && listaOpciones.head != "m") || !listaOpciones(1).forall(_.isDigit) || listaOpciones(1).toInt > 2 || listaOpciones(1).toInt < 1 || !listaOpciones(2).forall(_.isDigit) || !listaOpciones(3).forall(_.isDigit)) {
+    println("Error en la entrada de datos")
+    throw new Error("Error en la entrada de datos")
+  }
+  listaOpciones
 }
